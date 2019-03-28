a6485e16cf562df815bd595da22b7b32b4657db6
Stephen <36123293+sparce@users.noreply.github.com>
2019-03-28 11:46:24 +1100

    Add time left and buttons

--- DSBot.R
+++ DSBot.R
@@ -2,6 +2,12 @@ library(httr)
 library(rvest)
 library(purrr)
 library(jsonlite)
+library(future)
+
+future::plan("multiprocess")
+
+token <- Sys.getenv("SLACK_BOT_TOKEN")
+
 
 #* @post /exercise
 #* @serializer unboxedJSON
@@ -13,43 +19,46 @@ function(req, res, channel_id, text, trigger_id) {
     return(list(text = "Please try again with the link to an episode."))
   }
   
-  challenges <- GET(episode) %>% 
-    content %>% 
-    html_nodes("blockquote") %>% 
-    keep(~html_attrs(.) == "challenge")
-
-  challenge_ids <- html_node(challenges, "h2") %>% html_attr("id")
-  challenge_names <- html_node(challenges, "h2") %>% html_text()
-  
-  map2(challenge_names, challenge_ids, function(nm, id) {
-    list(label = nm, value = id)
-  })
-   
-  body <- list(
-    trigger_id = trigger_id,
-    dialog = list(
-      title = "Start an exercise",
-      callback_id = "exercise_callback",
-      state = episode,
-      elements = list(
-        list(
-          type = "select",
-          name = "challenge",
-          label = "Which challenge?",
-          options = map2(challenge_names, challenge_ids, function(nm, id) {
-            list(label = nm, value = id)
-          })
-        ),
-        list(
-          type = "text",
-          name = "time",
-          label = "How long to run (in minutes)?"
+  f <- future({
+    challenges <- GET(episode) %>% 
+      content %>% 
+      html_nodes("blockquote") %>% 
+      keep(~html_attr(., "class") == "challenge")
+    
+    challenge_ids <- html_node(challenges, "h2") %>% html_attr("id")
+    challenge_names <- html_node(challenges, "h2") %>% html_text()
+    
+    map2(challenge_names, challenge_ids, function(nm, id) {
+      list(label = nm, value = id)
+    })
+    
+    body <- list(
+      trigger_id = trigger_id,
+      dialog = list(
+        title = "Start an exercise",
+        callback_id = "exercise_callback",
+        state = episode,
+        elements = list(
+          list(
+            type = "select",
+            name = "challenge",
+            label = "Which challenge?",
+            options = map2(challenge_names, challenge_ids, function(nm, id) {
+              list(label = nm, value = id)
+            })
+          ),
+          list(
+            type = "text",
+            subtype = "number",
+            name = "time",
+            label = "How long to run (in minutes)?"
+          )
         )
       )
-    )
-  )  
-  
-  r = POST("https://slack.com/api/dialog.open",add_headers("Authorization" = "Bearer xoxp-587828095701-588568660775-588633919799-bf4ed98866dcf2ab7bfda054c430f827"), body = body, encode = "json")
+    )  
+    
+    r = POST("https://slack.com/api/dialog.open",add_headers("Authorization" = glue::glue("Bearer {token}")), body = body, encode = "json")
+  })
   
   res$status <- 200
   return(res)
@@ -61,9 +70,9 @@ function(req, res, payload) {
   res$status <- 200
   
   payload <- jsonlite::fromJSON(payload)
-  payload
+  #payload
   
-  saveRDS(object = payload, file = "payload.rds")
+  #saveRDS(object = payload, file = "payload.rds")
   
   # body <- list(
   #   response_type = "ephemeral",
@@ -91,7 +100,7 @@ function(req, res, payload) {
   # 
   # POST(payload$response_url, body = body, encode = "json")
   
-  system(paste0("Rscript manage_exercise.R"," '", toJSON(payload, auto_unbox = T), "'"), wait = F, ignore.stdout = T, ignore.stderr = T)
+  f %<-% system(paste0("Rscript manage_exercise.R"," '", toJSON(payload, auto_unbox = T), "'"), wait = F, ignore.stdout = T, ignore.stderr = T)
   
   return(res)
 }
--- manage_exercise.R
+++ manage_exercise.R
@@ -2,17 +2,19 @@ library(purrr)
 library(jsonlite)
 library(httr)
 library(rvest)
+library(lubridate)
+
+token <- Sys.getenv("SLACK_BOT_TOKEN")
+
 payload <- commandArgs(trailingOnly = T) %>% fromJSON()
 
-browser()
-
-print(payload)
+end_time <- now() + minutes(payload$submission$time)
 
 ep <- GET(payload$state) %>% content()
 
 challenges <- ep %>% 
   html_nodes("blockquote") %>% 
-  keep(~html_attrs(.) == "challenge")
+  keep(~html_attr(., "class") == "challenge")
 
 running_challenge <- challenges[(html_node(challenges, "h2") %>% html_attr("id")) == payload$submission$challenge]
 
@@ -22,9 +24,16 @@ challenge_text <- running_challenge %>%
   glue::glue_collapse("\n")
 
 
+time_left <- function() {
+  secs_left <- difftime(end_time, now(), units = "sec") %>% as.integer()
+  
+  glue::glue("{floor(secs_left / 60)}:{sprintf('%0.2d', secs_left%%60)}")
+}
+
 message <- list(
   response_type = "in_channel",
-  text = "fallback_text",
+  channel = payload$channel$id,
+  text = challenge_text,
   blocks = list(
     list(
       type = "section",
@@ -32,8 +41,82 @@ message <- list(
         type = "mrkdwn",
         text = challenge_text
       )
+    ),
+    list(
+      type = "context",
+      elements = list(
+        list(
+          type = "mrkdwn",
+          text = glue::glue("*Time left:* {time_left()}")
+        )
+      )
+    ),
+    list(
+      type = "actions",
+      elements = list(
+        list(
+          type = "button",
+          text = list(
+            text = ":thumbsup: Completed",
+            type = "plain_text",
+            emoji = T
+          ),
+          value = "finished"
+        ),
+        list(
+          type = "button",
+          text = list(
+            text = ":sos: I need help",
+            type = "plain_text",
+            emoji = T
+          ),
+          value = "help"
+        )
+      )
     )
   )
 )
 
-POST(payload$response_url, body = message, encode = "json")
+resp <- POST("https://slack.com/api/chat.postMessage",add_headers("Authorization" = glue::glue("Bearer {token}")), body = message, encode = "json")
+
+saveRDS(resp, "resp.rds")
+
+#Update message with time remaining for challenge
+while(end_time > now()) {
+  
+  old_message <- content(resp)
+  
+  #Update time left
+  new_blocks <- old_message$message$blocks
+  new_blocks[[2]]$elements[[1]]$text <- glue::glue("*Time left:* {time_left()}")
+  
+  updated_message <- list(
+    channel = old_message$channel,
+    text = old_message$message$text,
+    ts = old_message$ts,
+    blocks = new_blocks
+  )
+  
+  resp <- POST("https://slack.com/api/chat.update",add_headers("Authorization" = glue::glue("Bearer {token}")), body = updated_message, encode = "json")
+  
+  Sys.sleep(5)
+
+}
+
+
+#post final message
+old_message <- content(resp)
+
+#Update time left
+new_blocks <- old_message$message$blocks
+new_blocks[[2]]$elements[[1]]$text <- "*Exercise complete*"
+
+updated_message <- list(
+  channel = old_message$channel,
+  text = old_message$message$text,
+  ts = old_message$ts,
+  blocks = new_blocks
+)
+
+resp <- POST("https://slack.com/api/chat.update",add_headers("Authorization" = glue::glue("Bearer {token}")), body = updated_message, encode = "json")
+
